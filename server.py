9a4ae75103704d6d9cceb37c6262ad76641f9310
Vitaliy <rakitin.v.pa@sberbank.ru>
2019-12-18 14:34:56 +0300

    fix

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import render_template
 
+from flask.cli import FlaskGroup
 
 app = Flask(__name__)
+cli = FlaskGroup(app)
 
 
 @app.route('/', methods=['GET'])
@@ -12,4 +14,5 @@ def root():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=9084, threaded=True)
+    # app.run(host="0.0.0.0", port=9084, threaded=True)
+    cli()
