f0fb8ea82f2cf23e6fb0516981650ea342870797
Vitaliy <rakitin.v.pa@sberbank.ru>
2020-04-08 00:22:55 +0300

    server

---
+++ nginx/website/precache-manifest.8d5e9fdbb97ba159f70280e9b00a4b97.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df97994565ee0b78bc8e0bf8e3aad6e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f8729d708c390461bd1",
+    "url": "/static/css/2.a90a6225.chunk.css"
+  },
+  {
+    "revision": "d7bd5272e9700d81b74b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8f8729d708c390461bd1",
+    "url": "/static/js/2.f21ede29.chunk.js"
+  },
+  {
+    "revision": "7bfc831da88901f9302a0bb945eb618f",
+    "url": "/static/js/2.f21ede29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7bd5272e9700d81b74b",
+    "url": "/static/js/main.0836eb96.chunk.js"
+  },
+  {
+    "revision": "27fead4c70fc38b0920e",
+    "url": "/static/js/runtime-main.a4b3455f.js"
+  },
+  {
+    "revision": "afd16a2708457d7fb1bbd8b6d4e38bf3",
+    "url": "/static/media/robo.afd16a27.png"
+  }
+]);
--- website/app.py
+++ website/app.py
@@ -1,11 +1,14 @@
 from flask import Flask, request, jsonify
-from flask import render_template
+from flask import render_template, Response
 from flask.cli import FlaskGroup
 from log import logger
-from ya import find_ticket
+from requests import post
+# from flask_cors import CORS
+
 app = Flask(__name__)
 cli = FlaskGroup(app)
 SECRET_KEY = ""
+
 
 @app.route('/', methods=['GET'])
 def root():
@@ -14,26 +17,16 @@ def root():
     return render_template('index.html', link=link, text=text)
 
 
-@app.route('/google', methods=['GET'])
-def google():
-    link = "https://google.com/"
-    text = "Заходите в гугл"
-    return render_template('index.html', link=link, text=text)
-
-
-@app.route('/yandex_rasp', methods=['GET'])
-def yandex_rasp():
-    logger.info("GET YANDEX: {0}".format(request.args))
+@app.route('/proxy', methods=['POST'])
+def proxy_post():
+    logger.info("PROXY POST: {0}".format(request.args))
     key = request.args.get('key')
     if key != SECRET_KEY:
         return jsonify({"status": False})
 
-    from_station = request.args.get('from')
-    to_station = request.args.get('to')
-    date = request.args.get('date')
-    amount = request.args.get('amount')
-    result = find_ticket(from_station, to_station, date, amount)
-    logger.info("RESULT: {0}".format(result))
+    url = request.args.get('url')
+    result = post(url, request.data, request.headers).json()
+    logger.info("PROXY result: {0}".format(result))
     return jsonify(result)
 
 
